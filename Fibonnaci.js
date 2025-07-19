b572d0fd90e1a52e3f4a66c323fc923f19e1522b
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-07-19 10:05:57 +0800

    Solved Fibonnaci sequence using iterative and recursive solution

---
+++ Fibonnaci.js
@@ -0,0 +1,34 @@
+
+// Iterative Solution
+
+function fibs(number){
+    const FibArray = [0, 1];
+
+    if(number == 1) {
+        return [0]
+    }
+
+    for(let num = number; num > 2; num--){
+        FibArray.push((FibArray.at(-1) + FibArray.at(-2)));
+    }
+
+    return FibArray;
+}
+
+// Recursive Solution
+
+function fibsRec(number){
+
+    if (number == 1) {
+        return [0]
+    }else if (number == 2){
+        return [0, 1]
+    }
+
+    const FibArray = fibsRec(number - 1);
+    FibArray.push(FibArray[FibArray.length - 1] + FibArray[FibArray.length - 2]);
+    return FibArray
+}
+
+console.log(fibs(10));
+console.log(fibsRec(10));
